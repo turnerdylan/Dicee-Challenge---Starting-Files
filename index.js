2c1852da1c16fecdd7003e76e3fd21828e454297
Dylan <turner.t.dylan@gmail.com>
2023-09-01 20:23:03 -0600

    Dice site first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+var title = document.querySelector("h1");
+
+document.querySelector("img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png")
+document.querySelector("img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png")
+
+if(randomNumber1 > randomNumber2){
+    title.innerHTML = "Player one wins! <-";
+} else if (randomNumber1 == randomNumber2){
+    title.innerHTML = "Tie game";
+} else {
+    title.innerHTML = "Player two wins! ->";
+}
